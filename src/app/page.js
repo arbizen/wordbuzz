04705d642793744ff79a501e9965e33a2d955cce
Arb Rahim Badsa <arbrahimbadsa@gmail.com>
2023-12-15 13:07:56 +0600

    feat: add landing page

--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,53 @@ import {
   CIRCLE_RADIUS,
 } from "@/components/wordboard/constants";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import XYZ from "@/components/svgs/xyz";
+import { Mesh1 } from "@/components/svgs/mesh";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen flex-col">
-      <Link href="/login">Start playing</Link>
+    <div className="h-screen w-screen overflow-y-scroll bg-[#101619]">
+      <header className="relative z-50 flex justify-between items-center h-[75px] px-[52px] border-b border-b-[#1D2539]">
+        <h3 className="text-[24px] text-slate-100 font-black">Wordbuzz</h3>
+        <Button asChild variant="link" className="text-white text-base">
+          <Link href="/login">Login</Link>
+        </Button>
+      </header>
+      <div className="relative px-[90px]">
+        <div className="relative h-[calc(100vh_-_75px)] w-full flex items-center z-50">
+          <div className="w-full flex flex-col gap-[36px]">
+            <h1 className="text-[60px] font-black text-slate-100 max-w-[650px] leading-tight">
+              Never match a word alone
+            </h1>
+            <div className="max-w-[736px] text-blue-200 text-2xl font-normal leading-[33.11px]">
+              Enough being scared of vocabularies? Well, now you got a way to
+              scare your friend too. Start a vocabulary battle now!
+            </div>
+            <Button asChild className="max-w-[165px] h-[50px]">
+              <Link href="/login">Start battle</Link>
+            </Button>
+          </div>
+        </div>
+        <div className="w-auto h-[116px] relative">
+          <div className="w-[375px] h-[116px] pr-12 left-0 top-0 absolute border-r border-slate-800 flex-col justify-start items-start gap-2.5 inline-flex">
+            <div className="text-white text-6xl font-black">100K </div>
+            <div className="text-blue-200 text-2xl font-normal leading-[33.11px]">
+              Match from over 100K words
+            </div>
+          </div>
+          <div className="w-[328px] h-[116px] px-12 left-[377px] top-0 absolute flex-col justify-start items-start gap-2.5 inline-flex">
+            <div className="text-white text-6xl font-black">2K</div>
+            <div className="text-blue-200 text-2xl font-normal leading-[33.11px]">
+              Join 2K+ word gurus
+            </div>
+          </div>
+        </div>
+
+        <XYZ className="absolute right-0 top-[58px] z-40" />
+        <Mesh1 className="absolute left-0 right-0 top-0 z-40" />
+      </div>
     </div>
   );
 }
---
+++ src/components/svgs/mesh.js
@@ -0,0 +1,46 @@
+export const Mesh1 = ({ className }) => {
+  return (
+    <svg
+      width="1157"
+      height="976"
+      viewBox="0 0 1157 976"
+      className={className}
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g opacity="0.2" filter="url(#filter0_f_92_387)">
+        <ellipse
+          cx="575.644"
+          cy="416.343"
+          rx="502.5"
+          ry="147"
+          transform="rotate(43.0279 575.644 416.343)"
+          fill="#2563EB"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_f_92_387"
+          x="-5.23822"
+          y="-143.081"
+          width="1161.77"
+          height="1118.85"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="BackgroundImageFix"
+            result="shape"
+          />
+          <feGaussianBlur
+            stdDeviation="100"
+            result="effect1_foregroundBlur_92_387"
+          />
+        </filter>
+      </defs>
+    </svg>
+  );
+};
