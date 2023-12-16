49c050bc1b7978212f96e786d10e526bd2707df0
Arb Rahim Badsa <arbrahimbadsa@gmail.com>
2023-12-16 23:14:15 +0600

    refactor: remove comment

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -3,7 +3,6 @@ const URL = "https://wordbuzz.arbizen.com";
 export function GET() {
   const body = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
-    <!--We manually set the two URLs we know already-->
     <url>
       <loc>${URL}</loc>
     </url>
